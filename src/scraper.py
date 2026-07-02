7a6d7776ec49f2a4fc46a2ce7439bc4a760f3893
Schizo <22307673@student.ciu.edu.tr>
2026-07-02 05:06:38 +0300

    Update scraper.py

--- src/scraper.py
+++ src/scraper.py
@@ -1,3 +1,23 @@
-def fetch_data():
-    print("تم تشغيل سكربت السحب بنجاح! جاهز لجلب البيانات...")
-    return ["Headline 1", "Headline 2"]
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_financial_news():
+    url = "https://finance.yahoo.com/news/"
+   response =  requests.get(url)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    headlines = []
+    for item in soup.find_all('h3', limit=10):
+         title = item.get_text(strip=True)
+         headlines.append(title)
+   
+    return headlines 
+
+
+else:
+    print("connection faild")
+    return[]
+    
+    
+
+    
