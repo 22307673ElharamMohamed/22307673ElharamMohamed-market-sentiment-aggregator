3ee963b697bea7f5bd5f7a026e09ddef35feb925
Schizo <22307673@student.ciu.edu.tr>
2026-07-02 05:11:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from src.scraper import fetch_data
 
 if __name__ == "__main__":
-    print("جاري تشغيل نظام تحليل مشاعر السوق...")
-    data = fetch_data()
-    print(f"تم جلب البيانات بنجاح: {data}")
+    print("== news are loading==")
+    new = fetch_financial_news()
+    for index, headline in enumerate(news, 1):
+        print(f"{index}. {headlin}")
+    
+
+
