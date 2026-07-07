a9ad4d48277166fcb7ad5b4e428940eb28bc7a90
Schizo <22307673@student.ciu.edu.tr>
2026-07-07 22:50:22 +0300

    اكتمل المشروع بنجاح وإضافة دالة ربط قاعدة البيانات sqlite3

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from src.scraper import fetch_financial_news
 from src.analyzer import analyze_sentiment
+from src.database import init_db, save_news_sentiment
 
 if __name__ == "__main__":
-    print("=== جاري سحب الأخبار الحية والواقعية الآن... ===")
+    init_db()
 
+    print("=== جاري سحب الأخبار الحية والواقعية الآن... ===")
     news = fetch_financial_news()
 
     if news:
@@ -11,9 +13,11 @@ if __name__ == "__main__":
         for index, headline in enumerate(news, 1):
             label, score = analyze_sentiment(headline)
 
+            save_news_sentiment(headline,label, score)
+
 
             print(f"{index}. 📰 الخبر: {headline}")
             print(f"   📊 التحليل: {label} (الدرجة: {score})")
-            print("-" * 60)  # خط فاصل بين كل خبر وخبر
+            print("-" * 60)
     else:
         print("لم يتم العثور على أخبار، تأكدي من الكود أو الموقع.")
--- src/database.py
+++ src/database.py
@@ -1,1 +1,28 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("market_data.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS news_sentiment  (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    headline TEXT NOT NULL,
+    sentiment Text NOT NULL,
+    score REAL NOT NULL
+    )
+    """)
+    conn.commit()
+    conn.close()
+
+def save_news_sentiment(headline, sentiment, score):
+        conn = sqlite3.connect("market_data.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        INSERT INTO news_sentiment (headline, sentiment, score)
+            VALUES (?, ?, ?)
+            """, (headline, sentiment, score))
+
+        conn.commit()
+        conn.close()
 
